c01bcc1de68185043ebc87de11a73123900b7f10
Kirit Sælensminde <kirit@kirit.com>
2018-07-12 23:33:44 +0700

    Add example for writes.

---
+++ intro/writes.cpp
@@ -0,0 +1,32 @@
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+
+int main() {
+    std::vector<char> data(10 << 20);
+    std::ifstream("/dev/urandom", std::ios::binary).read(data.data(), data.size());
+    const auto write = [&data](std::size_t size) {
+        std::ofstream out("/dev/null", std::ios::binary);
+        const auto started = std::chrono::steady_clock::now();
+        std::size_t writes{};
+        for ( std::size_t i{}; i < data.size(); i += size ) {
+            out.write(&data[i], size);
+            ++writes;
+        }
+        const auto ended = std::chrono::steady_clock::now();
+        const auto ns = std::chrono::duration_cast<
+            std::chrono::nanoseconds>(ended - started).count();
+        std::cout << (size >> 10) << "KB chunks used " << writes <<
+            " writes taking " << (ns / 1000) << "us "
+            "(" << (1000.0 * ns / data.size()) << "ns per byte)" << std::endl;
+    };
+    /// Perform lots of small allocations
+    write(1 << 10);
+    /// Perform fewer bigger allocations
+    write(16 << 10); // 16 times bigger
+    /// Perform even bigger allocations
+    write(512 << 10); // 32 times bigger
+}
+
