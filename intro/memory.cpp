10b60bd897b39024c95ee4e62c1d628f24774f45
Kirit Sælensminde <kirit@kirit.com>
2018-07-12 23:34:00 +0700

    We don't need to loop if we just allocate more RAM.

--- intro/memory.cpp
+++ intro/memory.cpp
@@ -5,20 +5,18 @@
 
 int main() {
     const auto trial = [](const std::size_t size) {
-        const std::size_t target = 1 << 20; // 1 MB
+        const std::size_t target = 10 << 20; // 1 MB
         std::size_t allocs{};
         const auto started = std::chrono::steady_clock::now();
-        for ( auto times = 10; times; --times ) {
-            for ( auto left = target; left; left -= size ) {
-                if ( not malloc(size) ) std::exit(2);
-                ++allocs;
-            }
+        for ( auto left = target; left; left -= size ) {
+            if ( not malloc(size) ) std::exit(2);
+            ++allocs;
         }
         const auto ended = std::chrono::steady_clock::now();
         const auto ns = std::chrono::duration_cast<
             std::chrono::nanoseconds>(ended - started).count();
         std::cout << (size >> 10) << "KB per allocation. " <<
-            allocs << " allocations: " << ns / 1000 << "us (" << (ns / allocs) << "ns each). "
+            allocs  << " allocations: " << ns / 1000 << "us (" << (ns / allocs) << "ns each). "
             "Per byte: " << (1000.0 * ns / target) <<  "ps" << std::endl;
     };
     /// Perform lots of small allocations
