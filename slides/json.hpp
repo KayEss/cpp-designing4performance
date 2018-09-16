a87cb3f3d24bb200be963af3f695ba5180da5850
Kirit Sælensminde <k@kirit.com>
2018-09-16 16:08:04 +0700

    Start to put together slides with initial code.

---
+++ slides/json.hpp
@@ -0,0 +1,21 @@
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+
+
+struct json {
+using array_type = std::vector<std::shared_ptr<json>>;
+using object_type = std::unordered_map<std::string, std::shared_ptr<json>>;
+using value_type = std::variant<
+    std::monostate,
+    bool, int64_t, double,
+    std::string,
+    array_type. object_type>;
+
+    value_type value;
+};
+
+
+std::string stringify(const json &);
---
+++ slides/stringify.cpp
@@ -0,0 +1,30 @@
+#include "json.hpp"
+
+struct stringer {
+    std::string operator () (std::monostate) {
+        return "null";
+    }
+    std::string operator () (bool b) {
+        return b ? "true" : "false";
+    }
+    std::string operator () (int64_t i) {
+        return std::to_string(i);
+    }
+    std::string operator () (double d) {
+        return std::to_string(d);
+    }
+    std::string operator () (const std::string &s) {
+        return s;
+    }
+    std::string operator () (const json::object_type &o) {
+        return "{}";
+    }
+    std::string operator () (const json::array_type &a) {
+        return "[]";
+    }
+};
+
+std::string stringify(const json &v) {
+    return std::visit(stringer{}, v.value);
+}
+
