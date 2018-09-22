375f949c394a0b35e18991556f3e09d87e4f37f8
Kirit Sælensminde <k@kirit.com>
2018-09-22 15:31:39 +0700

    More code and start slides for last part of talk.

--- slides/select.buffer.cpp
+++ slides/select.buffer.cpp
@@ -8,21 +8,16 @@ void buffered() {
     std::vector<std::vector<json>> rs;
 
 std::string buffer;
-buffer.reserve(64 << 10);
-for ( auto &row : rs ) {
+for ( const auto &row : rs ) {
     bool first{true};
     for ( const json &field : row ) {
-        if ( not first ) {
-            buffer += ',';
-        } else {
-            first = false;
-        }
+        if ( not first ) buffer += ',';
+        else first = false;
         stringify(field, buffer);
     }
     buffer += '\n';
     if ( buffer.size() > (60 << 10) ) {
-        std::cout << buffer;
-        buffer.clear();
+        std::cout << buffer; buffer.clear();
     }
 }
 std::cout << buffer;
